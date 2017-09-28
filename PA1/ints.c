b677e103a714f356b0ed8c89092a3058ee598f50
Alexander Laswell <alaswell@baton-rouge.cs.colostate.edu>
2017-09-28 01:34:49 -0600

    working ints.c everything is correct however; need to verify "either number" means the user nums and not the evaluted numbers [1/2 and abs(1-2)]

--- PA1/ints.c
+++ PA1/ints.c
@@ -5,6 +5,8 @@
 // 	    facts about the numbers
 // Input: two (2) integer numbers where abs(x) < 1000000
 // Output: prints "Two numbers, please:\nDividing num1/num2:\nPos diff: diff\nDivisibile: x\n"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 	long num1, num2;	// to store the user input
@@ -12,14 +14,35 @@ int main() {
 	// Prompt the user for the numbers
 	// and read into the vars
 
-	printf("Two numbers, please:\n");
-	scanf("%f", num1); 
-	scanf("%f", num2); 
+	printf("Two numbers, please: ");
+	scanf("%ld", &num1); 
+	scanf("%ld", &num2); 
 
 	//Divide
 	if(num2 == 0)
-		printf("Dividing %f/%f: against the rules.", num1, num2);
+		printf("Dividing %ld/%ld: against the rules.\n", num1, num2);
 	else
-		printf("Dividing %f/%f: %f", num1, num2, num1/num2);
+		printf("Dividing %ld/%ld: %f\n", num1, num2, (float) num1/num2);
+
+	//Diff
+	printf("Pos diff: %ld\n", labs(num1-num2));
+
+	//42?
+	if(num1 == 42 || num2 == 42)
+		printf("It's the answer!\n");
+
+	//Divisible by 6
+	if(num1 % 6 == 0) {
+		if(num2 % 6 == 0)
+			// both
+			printf("Divisible: both\n");
+		else // only num1
+			printf("Divisible: only %ld\n", num1);
+	}
+	else if(num2 % 6 == 0) // only num2 
+		printf("Divisible: only %ld\n", num2);
+	else // neither
+		printf("Divisible: neither\n");
+
 	return 0;
 }
