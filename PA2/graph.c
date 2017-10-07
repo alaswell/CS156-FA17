59ab2805946e82ccd7ea58735567f2f454e85bdf
alaswell <alexlaswell86@gmail.com>
2017-10-06 23:51:25 -0600

    New rendtion of graph.c cannot use arrays or strings or anything worth using bc this is an entry level class :(

--- PA2/graph.c
+++ PA2/graph.c
@@ -1,36 +1,63 @@
-// CS156 HW2
-// Author: Alex Laswell
+// CS156    HW2
+// Author:  Alex Laswell
 // Created: 10-4-2017
 // Purpose: reads five numbers from the user 
 // 	    and produces a bar graph of the  
 // 	    indicated values
-// Input: five (5) integer numbers where abs(x) < 1000000
-// Output: "Two numbers, please:"
-//   	   result of dividing first by second
-// 	   positive difference of the two numbers
-// 	   if either of the input numbers == 42
-// 	   which numbers are divisible by six
+// Input:   five (5) integer numbers where x >= 0
+// Output:  A bar graph of the input
+
 #include <stdio.h>
 
 int main() {
-	const char* crops[5]  = {"Beans", "Carrots", "Lettuce", "Peas", "Squash"};
-	int pounds[5];
+	int beans, carrots, lettuce, peas, squash;
+	// prompt the user for the amount of each 
+	printf("Pounds of Beans: ");
+	scanf("%d", &beans);
+	printf("Pounds of Carrots: ");
+	scanf("%d", &carrots);
+	printf("Pounds of Lettuce: ");
+	scanf("%d", &lettuce);
+	printf("Pounds of Peas: ");
+	scanf("%d", &peas);
+	printf("Pounds of Squash: ");
+	scanf("%d", &squash);
 
-	for(int i = 0; i < 5; i++) {
-		printf("Pounds of %s", crops[i]);
-		scanf("%d", &pounds[i]);
+	// Beans using for loop
+	printf("\nPounds of Beans:   ");
+	for(int i = 0; i < beans; i++) {
+		printf("B");
 	}
 
-	int i = 0;
-	while(i < 5) {
-		printf("%s:", crops[i]);
-		printf("\t");
-		int j = 0;
-		do {
-			printf("%c", crops[i][0]);
-			
-		} while(++j < pounds[i]);
+	// Carrots using while loop 
+	printf("\nPounds of Carrots: ");
+	int j = 0;
+	while(j < carrots) {
+		printf("C");
+		j++;
+	}
+
+	// Lettuce using do while
+	printf("\nPounds of Lettuce: ");
+	j = 0;
+	do {
+		// gotta check for 0
+		if(lettuce != 0)
+			printf("L");		
+	} while(++j < lettuce);
+
+	// Peas using while loop
+	printf("\nPounds of Peas:    ");
+	j = 0;
+	while(j < peas) {
+		printf("P");
+		j++;
+	}
+
+	// squash using for loop
+	printf("\nPounds of Squash:  ");
+	for(int i = 0; i < squash; i++) {
+		printf("S");
+	}
 	printf("\n");
-	i++;
-	}
-}	
+}
